1e4c7eaf3c4092cae468a85e778bb0fa8117d2bb
husanibragimov <ibragimovdev@tstu.uz>
2023-07-05 17:56:46 +0500

    add apps, lib and files

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,11 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # libs
+    # 'drf_yasg',
+    'import_export',
     'rest_framework',
     'rest_framework_simplejwt',
 
     # local apps
-    'apps.account'
+    'apps.account',
+    'apps.competition',
 ]
 
 MIDDLEWARE = [
@@ -124,7 +128,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
