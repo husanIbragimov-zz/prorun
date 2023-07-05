553c347ce463e74f3c888c02124b96d6d7e0bc23
husanibragimov <ibragimovdev@tstu.uz>
2023-07-05 17:58:23 +0500

    root urls

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('account/', include('apps.account.api.urls')),
+    path('competition/', include('apps.competition.api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
