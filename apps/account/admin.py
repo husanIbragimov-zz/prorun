08fd3f107dd75c026e462a9aaaf7dd6abbacb43b
husanibragimov <ibragimovdev@tstu.uz>
2023-07-07 01:10:33 +0500

    create admin panel

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from apps.account.models import Account
+from apps.account.models import Account, VerifyPhoneNumber
 
 
-admin.site.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('get_fullname', 'phone_number', 'gender', 'is_verified', 'date_login')
+    list_filter = ('gender', 'date_created')
+    date_hierarchy = 'date_created'
+    readonly_fields = ('date_login', 'date_created')
+
+
+admin.site.register(Account, AccountAdmin)
+admin.site.register(VerifyPhoneNumber)
