37bfe91244741011ab81da94e2ea74861bc201c4
husanibragimov <ibragimovdev@tstu.uz>
2023-07-07 00:57:48 +0500

    login view

--- apps/account/api/v1/views.py
+++ apps/account/api/v1/views.py
@@ -1,7 +1,13 @@
 import random
+
+from rest_framework.authtoken.models import Token
 from rest_framework import generics, status, authentication, permissions
+from rest_framework.permissions import IsAuthenticated
+
 from apps.account.models import Account, VerifyPhoneNumber
-from .serializers import RegisterSerializer
+from .permissions import IsOwnUserOrReadOnly
+from .serializers import RegisterSerializer, LoginSerializer, VerifyPhoneNumberRegisterSerializer, \
+    VerifyPhoneNumberSerializer, ChangePasswordSerializer
 from rest_framework.response import Response
 
 from .utils import verify
@@ -11,19 +17,44 @@ class RegisterAPIView(generics.GenericAPIView):
     serializer_class = RegisterSerializer
 
     def post(self, request):
-        user = Account.objects.filter(phone_number=request.data['phone_number'])
-        if user:
-            return Response({'message': 'User already exists'}, status=status.HTTP_409_CONFLICT)
         serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        phone_number = serializer.data['phone_number']
-        code = str(random.randint(100_000, 999_999))
-        if len(phone_number) == 17:
-            verify(phone_number, code)
+        if serializer.is_valid():
+            serializer.save()
+            phone_number = serializer.data['phone_number']
+            code = str(random.randint(100_000, 999_999))
+            # verify(phone_number, code) sms provider kelganida ishga tushadi
             VerifyPhoneNumber.objects.create(phone_number=phone_number, code=code)
-        data = {
-            'code': code
-        }
+            data = {
+                'code': code
+            }
+            return Response({'success': True, 'message': 'Please verify phone number', 'data': data},
+                            status=status.HTTP_201_CREATED)
+        return Response({'success': False, 'message': f'Error'},
+                        status=status.HTTP_400_BAD_REQUEST)
 
-        return Response({'success': True, 'message': 'Please verify phone number', 'data': data},
-                        status=status.HTTP_201_CREATED)
+
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+            user_data = serializer.data
+            phone_number = user_data['phone_number']
+            user = Account.objects.get(phone_number=phone_number)
+            code = str(random.randint(100_000, 999_999))
+            if VerifyPhoneNumber.objects.filter(phone_number=phone_number).first():
+                check = VerifyPhoneNumber.objects.get(phone_number=phone_number)
+                check.delete()
+            if len(phone_number) == 13:
+                # verify(phone_number, code) sms provider kelganda ishlaydi
+                VerifyPhoneNumber.objects.create(phone_number=phone_number, code=code)
+            if verify:
+                return Response({
+                    'success': True, 'message': 'Verification code was sent to your phon number', 'code': code,
+                    'tokens': user.tokens
+                }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'err': f'{e}'})
+
