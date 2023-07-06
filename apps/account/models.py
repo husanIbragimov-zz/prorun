19b111b73422399768d93ea4d3e130cb0652712c
husanibragimov <ibragimovdev@tstu.uz>
2023-07-07 01:10:04 +0500

    beauty response

--- apps/account/models.py
+++ apps/account/models.py
@@ -44,7 +44,7 @@ class Account(AbstractBaseUser, PermissionsMixin):
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True,
                                     unique=True)  # validators should be a list
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
-    gender = models.CharField(max_length=6, default='none', help_text='none, male, female', null=True)
+    gender = models.CharField(max_length=6, choices=GENDER, default='none', help_text='none, male, female', null=True)
     birthday = models.DateField(null=True, blank=True)
     tall = models.FloatField(null=True, blank=True)
     weight = models.FloatField(null=True, blank=True)
@@ -62,7 +62,7 @@ class Account(AbstractBaseUser, PermissionsMixin):
     def get_fullname(self):
         if self.first_name and self.last_name:
             return f'{self.first_name} {self.last_name}'
-        return f'No full name'
+        return f'{self.first_name}'
 
     @property
     def tokens(self):
@@ -87,4 +87,4 @@ class VerifyPhoneNumber(models.Model):
     code = models.CharField(max_length=10, verbose_name="Code")
 
     def __str__(self):
-        return self.phone_number
+        return f'📞 Phone number: {self.phone_number};  🔐 code: {self.code}'
