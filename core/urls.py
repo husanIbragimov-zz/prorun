f2487854528dbd5f019f47cde089f81bc15fcb0e
husanibragimov <ibragimovdev@tstu.uz>
2023-07-07 12:34:27 +0500

    add jwt token urls

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
+
 schema_view = get_schema_view(
     openapi.Info(
         title="ProRun uz",
@@ -35,11 +37,14 @@ schema_view = get_schema_view(
     permission_classes=[permissions.AllowAny],
 )
 
-
 urlpatterns = [
     # swagger
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    path('token/api/access/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/api/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
 
     # admin
     path('admin/', admin.site.urls),
