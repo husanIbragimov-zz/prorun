207d53ecb509f9306d177a7c88838e6c9f39ec3b
husanibragimov <ibragimovdev@tstu.uz>
2023-08-02 00:07:58 +0500

    rm form data

--- apps/account/api/v1/views.py
+++ apps/account/api/v1/views.py
@@ -16,23 +16,24 @@ from .utils import verify
 
 class RegisterAPIView(generics.GenericAPIView):
     serializer_class = RegisterSerializer
-    parser_classes = (MultiPartParser, FormParser)
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
+        try:
+            serializer = self.serializer_class(data=request.data)
+            serializer.is_valid(raise_exception=True)
             serializer.save()
             phone_number = serializer.data['phone_number']
             code = str(random.randint(100_000, 999_999))
-            # verify(phone_number, code) sms provider kelganida ishga tushadi
+            verify(phone_number, code)  # sms provider kelganida ishga tushadi
             VerifyPhoneNumber.objects.create(phone_number=phone_number, code=code)
             data = {
                 'code': code
             }
             return Response({'success': True, 'message': 'Please verify phone number', 'data': data},
                             status=status.HTTP_201_CREATED)
-        return Response({'success': False, 'message': f'Error'},
-                        status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'success': False, 'message': f'{e}'},
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginAPIView(generics.GenericAPIView):
